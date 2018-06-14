c8f5023476768f153781cab2e0ff385888e627bc
Kranthi Kumar Gangineni <35349861+KranthiKumarGangineni@users.noreply.github.com>
2018-06-14 17:18:31 -0500

    icp4

--- ICPs/ICP4/ToDo/script.js
+++ ICPs/ICP4/ToDo/script.js
@@ -4,13 +4,14 @@ app.controller('toDoController', function($scope) {
     // Default ToDo List
     $scope.todoList = [{todoText:'Jquery', status:'Pending'},{todoText:'Javascript', status:'Pending'},
         {todoText:'AngularJs', status:'Pending'}];
-    updateTotal();
+
+    updateAddTotal();
 
     $scope.addItem = function() {
         $scope.todoList.push({todoText:$scope.newitem, status:'Pending'});
         $scope.newitem = "";
         $("#item").focus();
-        updateTotal();
+        updateAddTotal();
     };
 
     // Modifying Pending to Done
@@ -18,15 +19,33 @@ app.controller('toDoController', function($scope) {
       angular.element(event.target).parent().append("<span class='label success'>Done!</span>");
         angular.element(event.target).parent().attr("class", 'completed');
         angular.element(event.target).remove();
-        updateTotal();
+        updateRemoveTotal();
     };
 
-    function updateTotal() {
-
-        var completed = $('.success').length;
-        var pending = $('.pending').length;
+    function updateAddTotal() {
+        var completed = 0;
+        var pending = 0;
+        $scope.todoList.forEach(function (value) {
+          if(value.status == 'Pending'){
+              pending = pending+1;
+          }else{
+              completed = completed+1;
+          }
+        });
         if (completed > 0 || pending > 0) {
             $scope.totalText = " Pending: " + pending + " Completed: " + completed;
+        }else{
+            $scope.totalText = " Pending: " + $scope.todoList.length + " Completed: " + completed;
+        }
+    };
+
+    function updateRemoveTotal() {
+        completed = $('.success').length;
+        pending = $('.pending').length;
+        if (completed > 0 || pending > 0) {
+            $scope.totalText = " Pending: " + pending + " Completed: " + completed;
+        }else{
+            $scope.totalText = " Pending: " + $scope.todoList.length + " Completed: " + completed;
         }
     };
 
